bb5e41610c69073173d5b99bd1def8fc8fb86c4f
Андрей Рыжко <ryzhkoandrey@gmail.com>
2024-11-04 18:12:12 +0200

    backup

--- js/discover-form.js
+++ js/discover-form.js
@@ -1,3 +1,5 @@
+// AUTOCOMPLETE
+
 const countryList = [
     'Afghanistan',
     'Albania',
@@ -273,3 +275,17 @@ const autoCompleteJS = new autoComplete({
     },
     searchEngine: 'strict', // strict | loose
 });
+
+// EASEPICK
+
+const picker = new easepick.create({
+    element: "#datepicker",
+    css: [
+        "https://cdn.jsdelivr.net/npm/@easepick/bundle@1.2.1/dist/index.css"
+    ],
+    zIndex: 10,
+    format: "HH:mm, DD/MM/YY",
+    plugins: [
+        "TimePlugin"
+    ]
+})
