f829ae66be1578e53f8c893e4a6ea23ec5e51673
Андрей Рыжко <ryzhkoandrey@gmail.com>
2024-11-05 18:38:55 +0200

    backup

--- js/main.js
+++ js/main.js
@@ -1,23 +1,10 @@
 // SWIPER
 
 const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'vertical',
-    loop: true,
-
-    // If we need pagination
-    pagination: {
-        el: '.swiper-pagination',
-    },
-
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-
-    // And if we need scrollbar
-    scrollbar: {
-        el: '.swiper-scrollbar',
-    },
+    slidesPerView: 4,
+    spaceBetween: 32,
+    // navigation: {
+    //     nextEl: '.swiper-button-next',
+    //     prevEl: '.swiper-button-prev',
+    // },
 });
